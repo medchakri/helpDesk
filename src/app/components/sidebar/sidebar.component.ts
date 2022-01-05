e02137ca87f2d930b0ebcd2e8c622fcdae6f5c03
MedChakri <mchakri362@gmail.com>
2022-01-05 11:32:32 +0100

    Fist one

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from "@angular/core";
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  rtlTitle: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+
+  {
+    path: "/tables",
+    title: "Projects",
+    rtlTitle: "الرموز",
+    icon: "icon-atom",
+    class: ""
+  },
+  {
+    path: "/dashboard",
+    title: "Tasks",
+    rtlTitle: "خرائط",
+    icon: "icon-pin",
+    class: "" },
+  {
+    path: "/notifications",
+    title: "Tickets",
+    rtlTitle: "إخطارات",
+    icon: "icon-bell-55",
+    class: ""
+  }
+];
+
+@Component({
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"]
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+    if (window.innerWidth > 991) {
+      return false;
+    }
+    return true;
+  }
+}
